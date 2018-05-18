13609faf5fd5714807dcd3712fb9e089f7f8ccb1
zilhua <andyzilhua@qq.com>
2018-05-18 14:38:26 +0800

    upload white paper

---
+++ demo.rs
@@ -0,0 +1,16 @@
+##fileFormat=RSv1.0
+##fileDate=20180518
+##content=GSAMD-24v1-0_20011747_A1.bpm
+##source=GSGT_1.9.4
+##generated=bioguoke
+##totalSNP=700078
+##reference=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+##INFO=<ID=ID,Number=1,Type=String,Description="dbSNP rs id",Source="dbsnp",Version="150">
+##INFO=<ID=CHROM,Number=1,Type=String,Description="chromosome name",Source="1000G",Version="hs37d5">
+##INFO=<ID=POS,Number=1,Type=Integer,Description="SNP position",Source="1000G",Version="hs37d5">
+##INFO=<ID=NAME,Number=1,Type=String,Description="illumina name",Source="illunima",Version="GSAMD-24v1">
+#ID	CHROM	POS	INFO	NA0001
+rs568121721	1	100292476	NAME=1:100292476;OTHER=xxx	AA
+rs768830171	X	9882120	NAME=X:9882120-C-T	TT
+rs371814332	Y	2750408	NAME=200610-173	--
+.	MT	10397	NAME=200610-22	AA
